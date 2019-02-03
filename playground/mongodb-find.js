e9a823fbbac04c49cee2f11eca1c0fc9f1aabf62
vibhorsingh11 <vibhorsinghcs@gmail.com>
2019-02-03 22:22:49 +0530

    Find Query Added

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -6,17 +6,26 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,client) => {
     return console.log('Unable to connect to MongoDb server');
   }
   console.log('Connected to MongoDb server');
+  const db = client.db('TodoApp')
 
-  client.collection('Todos').find({
-    _id: new ObjectID('5c55a9caceddd536ec167437')
-  }).toArray().then((docs) => {
-    console.log('Todos');
-    console.log(JSON.stringify(docs,undefined,2));
-  },(err) => {
-    console.log('Unable to fetch todos',err);
+  // db.collection('Todos').find({
+  //   _id: new ObjectID('5c55905a77a718095cd22a9c')
+  // }).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs,undefined,2));
+  // },(err) => {
+  //   console.log('Unable to fetch todos',err);
+  // });
+  //
+  // db.collection('Todos').find().count().then((docs) => {
+  //   console.log('Todos count: ${count}');
+  // },(err) => {
+  //   console.log('Unable to fetch todos',err);
+  // });
+
+  db.collection('users').find({name:'Vibhor'}).toArray().then((docs) => {
+    console.log(JSON.stringify(docs));
   });
-
-
 
   // client.close();
 })
