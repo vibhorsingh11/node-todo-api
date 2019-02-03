20e35e7aa44b5fa228023ea12f75e45072403114
vibhorsingh11 <vibhorsinghcs@gmail.com>
2019-02-04 00:15:55 +0530

    Added method to delete by ID

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -8,19 +8,25 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,client) => {
   console.log('Connected to MongoDb server');
   const db = client.db('TodoApp')
 
-//deleteMany
-  db.collection('Todos').deleteMany({text:'Eat Lunch'}).then((result) => {
-    console.log(result);
-  });
+// //deleteMany
+//   db.collection('Todos').deleteMany({text:'Eat Lunch'}).then((result) => {
+//     console.log(result);
+//   });
+//
+// //deleteOne
+//   db.collection('Todos').deleteOne({text:'Eat Lunch'}).then((result) => {
+//     console.log(result);
+//   });
+//
+// //findOneAndDelete
+//   db.collection('Todos').findOneAndComplete({text:'Eat Lunch'}).then((result) => {
+//     console.log(result);
+//   });
 
-//deleteOne
-  db.collection('Todos').deleteOne({text:'Eat Lunch'}).then((result) => {
-    console.log(result);
-  });
+  // db.collection('users').deleteMany({name: 'Luke'});
 
-//findOneAndDelete
-  db.collection('Todos').findOneAndComplete({text:'Eat Lunch'}).then((result) => {
-    console.log(result);
+  db.collection('users').findOneAndDelete({"_id" : ObjectID("5c57345e8ac5ba370a345cff")}).then((results) => {
+    console.log(JSON.stringify(results,undefined,2));
   });
 
   // client.close();
