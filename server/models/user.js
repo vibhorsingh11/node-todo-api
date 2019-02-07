0854ee2e8a03f4ad1791fb977647f7cab9c31209
vibhorsingh11 <vibhorsinghcs@gmail.com>
2019-02-07 12:14:39 +0530

    Added structure for Api

---
+++ server/models/user.js
@@ -0,0 +1,12 @@
+var mangoose = require('mangoose');
+
+var User = mongoose.model('User',{
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1
+  }
+});
+
+module.exports = {User};
